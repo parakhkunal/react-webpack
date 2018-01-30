2fb0be65174ce5d088c5f02dcbaf457f33641597
parakhkunal <parakh.kunal@gmail.com>
2018-01-29 17:09:37 -0800

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+// We are using node's native package 'path'
+// https://nodejs.org/api/path.html
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// Constant with our paths
+const paths = {
+  DIST: path.resolve(__dirname, 'dist'),
+  SRC: path.resolve(__dirname, 'src'),
+  JS: path.resolve(__dirname, 'src/js'),
+};
+
+// Webpack configuration
+module.exports = {
+  entry: path.join(paths.JS, 'app.js'),
+  output: {
+    path: paths.DIST,
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(paths.SRC, 'index.html'),
+    }),
+  ],
+// Dev server configuration -> ADDED IN THIS STEP
+  // Now it uses our "src" folder as a starting point
+  devServer: {
+    port: 8090
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
